4b05b19de52c8e36f9c5f1e6f10364e4fedca300
Hernan <kermit_cedillo@yahoo.com>
2021-10-13 15:31:15 -0700

    Have the user enter a name, when button is clicked name entered will display with a greeting

--- ShowMyName/app/src/main/java/edu/asu/cidse/se/showmyname/MainActivity.java
+++ ShowMyName/app/src/main/java/edu/asu/cidse/se/showmyname/MainActivity.java
@@ -3,12 +3,38 @@ package edu.asu.cidse.se.showmyname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button showButton;
+    private TextView nameText;
+    private EditText enterName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+        enterName = findViewById(R.id.editTextName);
+
+       showButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String name = enterName.getText().toString();
+               if(name.isEmpty()){
+                   nameText.setText("Hello, Bond");
+               }else {
+                   nameText.setText("Hello, " + name);
+               }
+
+           }
+       });
     }
+
+
+
+
 }
