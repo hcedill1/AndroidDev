5b2b60050f5935dd10ee50a32ebfeee160b8c59b
Hernan <kermit_cedillo@yahoo.com>
2021-10-13 21:17:01 -0700

    adding notes and a small examples doing true and not true Relational and Logical Operators

--- JavaPlayground/LearnJava/src/main/java/edu/asu/cidse/se/learnjava/MyClass.java
+++ JavaPlayground/LearnJava/src/main/java/edu/asu/cidse/se/learnjava/MyClass.java
@@ -3,6 +3,27 @@ package edu.asu.cidse.se.learnjava;
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello Hernan");
+
+        int a = 8;
+        int b = 90;
+
+        /*
+        -- Relational Operators --
+        != --> not equal
+        == -->equal
+        >= --> greater than or equal to
+        <= --> less than or equal to
+
+        -- Logical Operators--
+        ! => only when its by itself.
+         */
+
+        if(a == b){
+            System.out.println("They are equal");
+        }else if(a > b){
+            System.out.println("A is bigger than B");
+        }else{
+            System.out.println("B is bigger than A");
+        }
     }
 }
