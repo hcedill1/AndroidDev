971f5fd50f4c612f17dcb4dd96a391baf6286a23
Hernan <kermit_cedillo@yahoo.com>
2021-10-15 21:25:19 -0700

    notes

---
+++ JavaPlayground/AndroidArchitecture/src/main/java/edu/asu/cidse/se/androidarchitecture/Main.java
@@ -0,0 +1,24 @@
+package edu.asu.cidse.se.androidarchitecture;
+
+public class Main {
+
+    /**
+     * Android Platform Architecture
+     * What is Android exactly? it is a bunch of things and technologies stacks
+     *
+     * Android is open source. Anyone can costumize it. You can go ahead to create your own android
+     * operating system.
+     * Downside? Anyone can take the code and customize it to their liking. Thats why
+     * it is all over the world. Most of the times as developers fragmintation of android. Which there is
+     * different manufacturers.
+     *
+     * Platform Architecture:
+     * So what it has is linux kernel
+     * Hardware
+     * Ntaive libraries with Android runtime
+     * Java API framework
+     * System apps
+     *
+     * Android Studio Tour
+     */
+}
