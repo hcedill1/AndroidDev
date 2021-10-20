86fcffa1573024619a58198b084f49a7b3d07680
Hernan <kermit_cedillo@yahoo.com>
2021-10-19 17:44:50 -0700

    adding the final touches for bio app such as having the user input info and displaying it on a TextView

--- Bio/app/src/main/java/edu/asu/cidse/se/bio/MainActivity.java
+++ Bio/app/src/main/java/edu/asu/cidse/se/bio/MainActivity.java
@@ -2,13 +2,32 @@ package edu.asu.cidse.se.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText enterHobbies;
+    private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterHobbies = findViewById(R.id.enter_hobbies);
+        hobbies = findViewById(R.id.hobbies_text);
+    }
+
+    public void addHobbies(View view) {
+        hobbies.setText(String.format("Hobbies: %s", enterHobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
+
+        //we will hide keyboard when the button is clicked done
+        InputMethodManager inputMethodManager =(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
